272becc0c217e7f56f3a8b1a1fe22c9e86bdec6d
Jongkil Park <park108@gmail.com>
2023-02-08 17:15:20 +0900

    Add test code for getUsers and getUser

--- src/test/kotlin/com/oms/user/controller/UserControllerTest.kt
+++ src/test/kotlin/com/oms/user/controller/UserControllerTest.kt
@@ -1,30 +1,32 @@
 package com.oms.user.controller
 
-import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
-@WebMvcTest(UserController::class)
-@AutoConfigureRestDocs
-class UserControllerTest {
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+@SpringBootTest
+@Transactional
+class UserControllerTest(
+    @Autowired val userController: UserController
+//    , @Autowired val userRepository: UserRepository
+) {
 
     @Test
-    fun `GET users 200 ok`() {
+    fun `get users`() {
+        val result = userController.getUsers()
+        assertEquals("200 OK", result.statusCode.toString(), "OK")
+    }
 
-        // When
-        val result = mockMvc.perform(get("/"))
+    @Test
+    fun `get user`() {
+        val result = userController.getUsers()
 
-        // Then
-        result.andExpect(status().isOk)
-                .andExpect(MockMvcResultMatchers.content().string(containsString("Monitor")))
+        result.body?.forEach {
+            val user = it.id?.let { it1 -> userController.getUser(it1) }
+            assertNotNull(user)
+        }
     }
 }
