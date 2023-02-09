40f71812144e31c4310cb2473c37caa408e487d2
Jongkil Park <park108@gmail.com>
2023-02-09 21:36:18 +0900

    Add test code - create user but already exists

--- src/test/kotlin/com/oms/user/controller/UserControllerTest.kt
+++ src/test/kotlin/com/oms/user/controller/UserControllerTest.kt
@@ -1,5 +1,6 @@
 package com.oms.user.controller
 
+import com.oms.user.entity.User
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -28,5 +29,17 @@ class UserControllerTest(
             val user = it.id?.let { it1 -> userController.getUser(it1) }
             assertNotNull(user)
         }
+    }
+
+    @Test
+    fun `create user but already exists`() {
+        val user = User()
+
+        user.email = "park108@gmail.com"
+        user.name = "Jongkil Park"
+        user.password = "1q2w3e"
+
+        val result = userController.postUser(user)
+        assertEquals("409 CONFLICT", result.statusCode.toString(), "Conflict")
     }
 }
